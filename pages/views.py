babf7553a0b83c3b227f0fb88c31d9d100c2c700
fantamady <fms294@nyu.edu>
2020-05-22 18:58:07 -0400

    added feedback form

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib import messages
+from . forms import FeedbackForm
 # Create your views here.
 
 
 def index(request):
     return HttpResponse("Hello World")
+
+
+def feedback(request):
+    if request.method == "POST":
+        form = FeedbackForm
+        if form.is_valid():
+            form.save()
+            messages.add_message(request, messages.INFO, "Feedback Submitted.")
+            return redirect("feedback")
+        else:
+            form = FeedbackForm()
+        return render(request, "pages/feedback.html", {'form':form})
