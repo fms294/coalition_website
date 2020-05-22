c6bc2e9baa9c33f6d1c48a24fc4126871ef4978c
fantamady <fms294@nyu.edu>
2020-05-22 18:42:21 -0400

    added models

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Feedback(models.Model):
+    firstname = models.CharField(max_length=200)
+    lastname= models.CharField(max_length=100)
+    areacode = models.IntegerField()
+    phone_number = models.IntegerField()
+    email = models.EmailField(max_length=100)
+    may_contact = models.BooleanField()
+    feedback=models.TextField()
+    date=models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural= "Feedback"
+
+    def __str__(self):
+        return self.firstname + "-" + self.email
+
+
+class Membership(models.Model):
+    firstname = models.CharField(max_length=200)
+    lastname = models.CharField(max_length=100)
+    areacode = models.IntegerField()
+    phone_number = models.IntegerField()
+    email = models.EmailField(max_length=100)
+    profession=models.CharField(max_length=200)
+    country= models.CharField(max_length=50)
+
+    def __str__(self):
+        return  self.firstname + "-" + self.email
+
